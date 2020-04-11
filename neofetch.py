776bd90c3ca5bdf0d80ce0b289d20633c880a5e8
dante <zhivchikovv2@gmail.com>
2020-04-11 20:21:55 +0300

    color ascii

--- neofetch.py
+++ neofetch.py
@@ -101,26 +101,25 @@ def memory():
 
 
 TEMPLATE = """
-                           {hostname} 
-                           {hostname_sep}
+                           
          
-       a88888.             OS: {os_version}
-      d888888b.            Kernel: {kernel}
-      d888888b.            Cpu Architecture: {cpu}
-      8P"YP"Y88            Shell: {shell}
-      8|o||o|88            Uptime: {uptime}
-      8'    .88            Resolution: {size}
-      8`._.' Y8            Local IP: {local_ip}
-     d/      `8b.             
-   .dP   .     Y8b.
-   d8:'   "   `::88b.
-  d8"           `Y88b
- :8P     '       :888
-  8a.    :      _a88
-  ._/"Yaa_ :    .| 88P|                                                
-  \    YP"      `| 8P  `.
-  /     \._____.d|    .'
-  `--..__)888888P`._.'
+\033[92m       a88888.       {hostname} \033[0m
+\033[92m      d888888b.   \033[0m   {hostname_sep}
+\033[92m      d888888b.    \033[0m\033[93m   OS: {os_version}
+\033[92m      8P"YP"Y88     \033[0m\033[93m  Kernel:{kernel}
+\033[93m      8|o||o|88     \033[0m\033[93m  Cpu architecture: {cpu}
+\033[93m      8'    .88     \033[0m\033[93m  Shell: {shell}
+\033[93m      8`._.' Y8      \033[0m\033[93m Uptime: {uptime}  
+\033[93m     d/      `8b.   \033[0m\033[93m  Resolution: {size}         
+\033[91m   .dP   .     Y8b. \033[0m\033[93m  Local IP: {local_ip}
+\033[91m   d8:'   "   `::88b.
+\033[91m  d8"           `Y88b
+\033[95m :8P     '       :888
+\033[95m  8a.    :      _a88
+\033[95m  ._/"Yaa_ :    .| 88P|                                                
+\033[94m   \    YP"      `| 8P  `.
+\033[94m  /     \._____.d|    .'
+\033[94m  `--..__)888888P`._.'
                                                                                                          
 \033[30m███\033[0m\033[91m███\033[0m\033[92m███\033[0m\033[93m███\033[0m\033[94m███\033[0m\033[95m███\033[0m\033[96m███\033[0m\033[97m███\033[0m
 
